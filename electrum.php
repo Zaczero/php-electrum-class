d51cf1fc939afcaa8aa61e252bec42e3ed0b78c1
Kamil Monicz <kamil@monicz.pl>
2020-01-09 21:48:09 +0100

    Added DocBlock comments

--- electrum.php
+++ electrum.php
@@ -7,6 +7,16 @@ class Electrum {
 	private $_rpcport;
 	private $_rpchost;
 
+	/**
+	 * Initializes electrum class
+	 *
+	 * @param string $rpcuser User used for RPC connection
+	 * @param string $rpcpass Password used for RPC connection
+	 * @param string $rpchost (optional) Host used for RPC connection
+	 * @param int $rpcport (optional) Port used for RPC connection
+	 *
+	 * @return void
+	 */
 	public function __construct(string $rpcuser, string $rpcpass, string $rpchost = "localhost", int $rpcport = 7777) {
 		$this->_rpcurl = "http://$rpcuser:$rpcpass@$rpchost:$rpcport";
 		$this->_rpcuser = $rpcuser;
@@ -15,6 +25,15 @@ class Electrum {
 		$this->_rpchost = $rpchost;
 	}
 
+	/**
+	 * Executes electrum command using RPC connection.
+	 *
+	 * @param string $method Command to be executed
+	 * @param array $params (optional) Command parameters
+	 *
+	 * @throws Exception If curl execution fails
+	 * @return mixed JSON-decoded response (as array) from electrum
+	 */
 	public function curl(string $method, array $params = []) {
 		$data = [
 			"id" => "curltext",
@@ -38,14 +57,35 @@ class Electrum {
 		return json_decode($response, true)["result"];
 	}
 
+	/**
+	 * Converts bitcoin to satoshi unit.
+	 *
+	 * @param float $btc Amount in bitcoin
+	 *
+	 * @return float Amount in satoshi
+	 */
 	public static function btc2sat(float $btc) : float {
 		return $btc * 100000000;
 	}
 
+	/**
+	 * Coverts satoshi to bitcoin unit.
+	 *
+	 * @param float $sat Amount in satoshi
+	 *
+	 * @return float Amount in bitcoin
+	 */
 	public static function sat2btc(float $sat) : float {
 		return $sat / 100000000;
 	}
 
+	/**
+	 * Broadcasts the hex-encoded transaction (TX) to the network.
+	 *
+	 * @param string $tx Hex-encoded transaction (TX)
+	 *
+	 * @return string Transaction hash (TXID)
+	 */
 	public function broadcast(string $tx) : string {
 		$response = $this->curl("broadcast", [
 			"tx" => $tx,
@@ -54,12 +94,24 @@ class Electrum {
 		return $response;
 	}
 
+	/**
+	 * Generates a new receiving address.
+	 *
+	 * @return string Generated bitcoin address
+	 */
 	public function createnewaddress() : string {
 		$response = $this->curl("createnewaddress", []);
 
 		return $response;
 	}
 
+	/**
+	 * Gets the wallet balance.
+	 *
+	 * @param bool $confirmed_only (optional) Should we include only confirmed funds
+	 *
+	 * @return float Wallet balance
+	 */
 	public function getbalance(bool $confirmed_only = false) : float {
 		$response = $this->curl("getbalance", []);
 
@@ -71,6 +123,13 @@ class Electrum {
 		return $total;
 	}
 
+	/**
+	 * Returns recommended sat/byte fee rate for selected priority.
+	 *
+	 * @param float $fee_level (optional) Priority of the transaction where 0.0 is the lowest and 1.0 is the highest
+	 *
+	 * @return float Estimated sat/byte fee rate
+	 */
 	public function getfeerate(float $fee_level = 0.5) : float {
 		if ($fee_level < 0.0 || $fee_level > 1.0) throw new Exception("fee_level must be between 0.0 and 1.0");
 
@@ -81,6 +140,15 @@ class Electrum {
 		return floatval($response) / 1000;
 	}
 
+	/**
+	 * Iterates through all of the transactions which met the provided criteria and returns an array of addresses and total transaction value.
+	 *
+	 * @param int $min_confirmations (optional) Only include transaction with X confirmations or more
+	 * @param int $from_height (optional) Only include transaction since block X (inclusive)
+	 * @param int|null $last_height (optional) Returns last processed block height (if any)
+	 *
+	 * @return array An array of receiving addresses and total transactions value
+	 */
 	public function history(int $min_confirmations = 1, int $from_height = 1, &$last_height) : array {
 		$result = [];
 		$response = json_decode($this->curl("history", [
@@ -104,6 +172,13 @@ class Electrum {
 		return $result;
 	}
 
+	/**
+	 * Checks if provided address is owned by the current wallet.
+	 *
+	 * @param string $address Address to check
+	 *
+	 * @return bool
+	 */
 	public function ismine(string $address) : bool {
 		$response = $this->curl("ismine", [
 			"address" => $address,
@@ -112,6 +187,15 @@ class Electrum {
 		return $response;
 	}
 
+	/**
+	 * Generates and signs a new transaction with provided parameters.
+	 *
+	 * @param string $destination Destination address to send to
+	 * @param float $amount Amount to send in bitcoin unit
+	 * @param float $amount_fee (optional) Fee amount in bitcoin unit (0.0 for dynamic)
+	 *
+	 * @return string Hex-encoded transaction (TX) ready to broadcast
+	 */
 	public function payto(string $destination, float $amount, float $amount_fee = 0.0) : string {
 		if ($amount <= 0) return "";
 		if ($amount_fee >= 0.01) return "";
@@ -130,6 +214,14 @@ class Electrum {
 		return $response["hex"];
 	}
 
+	/**
+	 * Generates and signs a new transaction with provided parameters. Sends all funds available.
+	 *
+	 * @param string $destination Destination address to send to
+	 * @param float $amount_fee (optional) Fee amount in bitcoin unit (0.0 for dynamic)
+	 *
+	 * @return string Hex-encoded transaction (TX) ready to broadcast
+	 */
 	public function payto_max(string $destination, float $amount_fee = 0.0) : string {
 		if ($amount_fee >= 0.01) return "";
 
@@ -147,6 +239,13 @@ class Electrum {
 		return $response["hex"];
 	}
 
+	/**
+	 * Checks if provided address is valid or not.
+	 *
+	 * @param string $address Address to validate
+	 *
+	 * @return bool
+	 */
 	public function validateaddress(string $address) : bool {
 		$response = $this->curl("validateaddress", [
 			"address" => $address,
